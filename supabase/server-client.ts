4a78de0b5652cb27df9e370aed05ffcd417894fe
Arham Humayun <arham.humayun99@gmail.com>
2024-03-24 06:18:11 -0600

    add supabase lib

---
+++ supabase/server-client.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { createServerClient, CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export async function createSupabaseAppServerClient(serverComponent = false) {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          if (serverComponent) {
+            return
+          }
+          cookieStore.set({ name, value, ...options })
+        },
+        remove(name: string, options: CookieOptions) {
+          if (serverComponent) {
+            return
+          }
+          cookieStore.delete({ name, ...options })
+        },
+      },
+    }
+  );
+}
