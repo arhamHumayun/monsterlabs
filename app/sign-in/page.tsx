be52eee1b817deea42edec1e66f2a88d9d30da7d
Arham Humayun <arham.humayun99@gmail.com>
2024-08-06 14:59:25 -0600

    remove text from sign in

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -12,9 +12,6 @@ export default async function SignIn() {
 
   return (
     <div className="flex flex-col py-10">
-      <h1 className='text-2xl font-semibold text-center mb-5'>
-        Create an account to start creating!
-      </h1>
       <LoginButton />
     </div>
   )
