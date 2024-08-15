60bbd8cc63e1f2bf853e60e16568051606e3d960
Arham Humayun <arham.humayun99@gmail.com>
2024-08-14 20:38:35 -0600

    add pagination to the bottom too

--- app/browse/[page]/page.tsx
+++ app/browse/[page]/page.tsx
@@ -1,5 +1,5 @@
 import { Separator } from '@/components/ui/separator';
-import {  getCountOfCreatures } from '../../actions';
+import { getCountOfCreatures } from '../../actions';
 import {
   Pagination,
   PaginationContent,
@@ -16,7 +16,6 @@ export default async function AllMonsters({
 }: {
   params: { page: string };
 }) {
-
   const creatureCount = await getCountOfCreatures();
   const monstersPerPage = 89;
 
@@ -53,51 +52,60 @@ export default async function AllMonsters({
     }
 
     return buttons;
-  }
+  };
+
+  const paginationSection = () => {
+    return (
+      <Pagination className="my-4">
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              href={currentPage === 1 ? '' : `/browse/${currentPage - 1}`}
+            />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href={`/browse/1`}>1</PaginationLink>
+          </PaginationItem>
+          {currentPage > 4 && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+          {pageButtons()}
+          {currentPage < maxCreaturePage - 3 && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+          <PaginationItem>
+            <PaginationLink href={`/browse/${maxCreaturePage}`}>
+              {maxCreaturePage}
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              href={
+                currentPage === maxCreaturePage
+                  ? ''
+                  : `/browse/${currentPage + 1}`
+              }
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    );
+  };
 
   return (
     <div className="w-full max-w-5xl mx-auto flex min-h-screen flex-col px-4 sm:px-6 mb-4">
       <h1 className="text-lg font-semibold">All creatures</h1>
       <Separator className="mb-4" />
-
-      <Pagination className="mb-4" >
-        <PaginationContent>
-          <PaginationItem>
-            <PaginationPrevious href={
-              currentPage === 1 ? '' : `/browse/${currentPage - 1}`
-            } />
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationLink href={`/browse/1`} >1</PaginationLink>
-          </PaginationItem>
-          {
-            currentPage > 4 && (
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            )
-          }
-            {pageButtons()}
-
-          {
-            currentPage < maxCreaturePage - 3 && (
-              <PaginationItem>
-                <PaginationEllipsis />
-              </PaginationItem>
-            )
-          }
-          <PaginationItem>
-            <PaginationLink href={`/browse/${maxCreaturePage}`} >{maxCreaturePage}</PaginationLink>
-          </PaginationItem>
-          <PaginationItem>
-            <PaginationNext href={
-              currentPage === maxCreaturePage ? '' : `/browse/${currentPage + 1}`
-            } />
-          </PaginationItem>
-        </PaginationContent>
-      </Pagination>
-
-      <CreatureList pageNumber={currentPage} monstersPerPage={monstersPerPage} />
+      {paginationSection()}
+      <CreatureList
+        pageNumber={currentPage}
+        monstersPerPage={monstersPerPage}
+      />
+      {paginationSection()}
     </div>
   );
 }
